cd443b568ae4a41897d6abdf42d6085ffa9e5817
jasethomson <jasethomson31@gmail.com>
2019-08-19 06:21:35 -0700

    Completed view-average-grade

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,25 +1,47 @@
 import React from 'react';
 import Header from './header';
 import GradeTable from './grade-table';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      grades: []
+      grades: [],
+      average: 0
     };
+    this.avgGrade = null;
   }
+
   componentDidMount() {
     this.getGrades();
   }
+
+  componentDidUpdate() {
+    this.getAverageGrade();
+    if (this.state.average !== this.avgGrade) {
+      this.setState({ average: this.avgGrade });
+    }
+  }
+
   getGrades() {
     fetch('/api/grades')
       .then(res => res.json())
       .then(grades => this.setState({ grades }));
   }
+
+  getAverageGrade() {
+    const avgGradeArray = this.state.grades;
+    let gradeTotal = null;
+    for (var gradeIndex = 0; gradeIndex < avgGradeArray.length; gradeIndex++) {
+      gradeTotal += avgGradeArray[gradeIndex].grade;
+    }
+    this.avgGrade = Math.floor(gradeTotal / avgGradeArray.length);
+  }
+
   render() {
     return (
       <div className="container">
-        <Header text="Student Grade Table"/>
+        <Header text="Student Grade Table" averageHeader="Average Grade" average={this.avgGrade}/>
         <GradeTable grades={this.state.grades}/>
       </div>
     );
